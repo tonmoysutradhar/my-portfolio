56befd9a80770de69e906b380d76a529e3ac3034
tonmoysutradhar <tonmoykumar933@gmail.com>
2024-08-26 00:27:00 +0600

    Add Quality Services Section

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+window.onload = () => {
+  // $(selector).countMe(delay,speed)
+  $(".counter_box1").countMe(100, 200);
+  $(".counter_box2").countMe(100, 200);
+  $(".counter_box3").countMe(100, 200);
+  $(".counter_box4").countMe(100, 200);
+};
+
+
---
+++ assets/js/dependancy.js
@@ -0,0 +1,26 @@
+var counter_list = [
+  {
+    counter_amount: 14,
+    counter_unit: "",
+    counter_label_top: "Years of",
+    counter_label_bottom: "Experience",
+  },
+  {
+    counter_amount: 50,
+    counter_unit: "+",
+    counter_label_top: "Project",
+    counter_label_bottom: "Completed",
+  },
+  {
+    counter_amount: 2,
+    counter_unit: "K",
+    counter_label_top: "Happy",
+    counter_label_bottom: "Clients",
+  },
+  {
+    counter_amount: 15,
+    counter_unit: "+",
+    counter_label_top: "Partners",
+    counter_label_bottom: "",
+  },
+];
