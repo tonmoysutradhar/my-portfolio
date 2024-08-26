08c09ba860b4800e4260a23a7bcd3fed8e213b67
tonmoysutradhar <tonmoykumar933@gmail.com>
2024-08-26 22:53:31 +0600

    Add Client Stories Section

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,33 @@
 window.onload = () => {
   // $(selector).countMe(delay,speed)
-  $(".counter_box1").countMe(100, 200);
-  $(".counter_box2").countMe(100, 200);
-  $(".counter_box3").countMe(100, 200);
-  $(".counter_box4").countMe(100, 200);
+  try {
+    $(".counter_box1").countMe(100, 200);
+    $(".counter_box2").countMe(100, 200);
+    $(".counter_box3").countMe(100, 200);
+    $(".counter_box4").countMe(100, 200);
+  } catch (error) {}
+
+  // Configure your options
+  try {
+    new Filterizr(".filter-container", {});
+  } catch (error) {
+    console.log(error);
+  }
+
+  try {
+    $(".owl-carousel").owlCarousel({
+      items: 2,
+      dots: true,
+      // nav: true,
+      autoplay: true,
+      animateOut: "animate__backOutLeft",
+      loop: true,
+      navText: [
+        '<button class="my_banner_nav">prev</button>',
+        "<button>next</button>",
+      ],
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
-
-
